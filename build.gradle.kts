791c3193ffacafe99079f8171fed1cb7cb7c8de2
Hayden Taylor <hayden@avuna.net>
2024-12-05 12:16:29 -0500

    Added reflections and JMH dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("java-library")
     id("application")
     id("me.champeau.jmh") version "0.7.2"
 }
@@ -15,8 +16,13 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 
+    implementation("org.openjdk.jmh:jmh-core:1.37")
+    annotationProcessor("org.openjdk.jmh:jmh-generator-annprocess:1.37")
+
     compileOnly("org.projectlombok:lombok:1.18.36")
     annotationProcessor("org.projectlombok:lombok:1.18.36")
+
+    implementation("org.reflections:reflections:0.10.2")
 }
 
 application {
@@ -29,4 +35,10 @@ tasks.test {
 
 tasks.withType<JavaCompile>().configureEach {
     options.compilerArgs.add("--enable-preview")
+}
+
+jmh {
+    warmupIterations = 2
+    iterations = 2
+    fork = 2
 }
