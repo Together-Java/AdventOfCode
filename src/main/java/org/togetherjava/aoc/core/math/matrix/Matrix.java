54903d3643bf3b7121f82d54edb26ce1b6ad482b
Madi Wilder <mattsmathclassofficial@gmail.com>
2025-06-07 14:13:49 -0500

    Add BinaryMatrix, update Matrix api

--- src/main/java/org/togetherjava/aoc/core/math/matrix/Matrix.java
+++ src/main/java/org/togetherjava/aoc/core/math/matrix/Matrix.java
@@ -3,6 +3,7 @@ package org.togetherjava.aoc.core.math.matrix;
 
 import org.togetherjava.aoc.core.math.Direction;
 import org.togetherjava.aoc.core.utils.AocUtils;
+import org.togetherjava.aoc.core.utils.Tuple;
 
 import java.util.*;
 import java.util.stream.IntStream;
@@ -22,6 +23,31 @@ public class Matrix<T> implements Iterable<T>, IMatrix<T> {
 
     public Matrix() {
         this(0, 0);
+    }
+
+    /**
+     * @param target Anything that <code>equals()</code> this will be a <code>true</code> entry, otherwise <code>false</code>
+     */
+    public BinaryMatrix toBinaryMatrix(T target) {
+        BinaryMatrix matrix = new BinaryMatrix(this, false);
+        for (var entry : this.getEntries()) {
+            if (Objects.equals(entry.value, target)) {
+                matrix.set(entry.row, entry.col, true);
+            }
+        }
+        return matrix;
+    }
+
+    public List<T> walkUnsafe(MatrixPosition start, String directions) {
+        MatrixPosition current = start.move(0, 0);
+        List<T> path = new ArrayList<>();
+        path.add(get(current));
+        for (char c : directions.toCharArray()) {
+            var dir = Direction.ofASCII(c);
+            current = current.move(dir);
+            path.add(get(current));
+        }
+        return path;
     }
 
     public Matrix(T[][] array) {
@@ -129,6 +155,50 @@ public class Matrix<T> implements Iterable<T>, IMatrix<T> {
     @Override
     public MatrixIterator iterator() {
         return new MatrixIterator();
+    }
+
+    public Direction findAdjacent(MatrixPosition startPos, T target) {
+        for (var dir : Direction.getCardinal()) {
+            Optional<T> preview = this.tryGet(startPos.move(dir));
+            if (preview.isPresent() && preview.get().equals(target)) {
+                return dir;
+            }
+        }
+        return null;
+    }
+
+    public Direction findAdjacentFacing(MatrixPosition startPos, T target, Direction facing) {
+        List<Direction> options = List.of(facing, facing.rotateLeft(), facing.rotateRight());
+        for (var dir : options) {
+            Optional<T> preview = this.tryGet(startPos.move(dir));
+            if (preview.isPresent() && preview.get().equals(target)) {
+                return dir;
+            }
+        }
+        return null;
+    }
+
+    public List<Matrix.Entry<T>> scanTaxicabBorder(MatrixPosition source, int distance) {
+        List<Matrix.Entry<T>> entries = new ArrayList<>();
+        for (int dr = -distance; dr <= distance; ++dr) {
+            int dc = distance - Math.abs(dr);
+            getEntry(source.move(dr, dc)).ifPresent(entries::add);
+            if (dc != 0) {
+                getEntry(source.move(dr, -dc)).ifPresent(entries::add);
+            }
+        }
+        return entries;
+    }
+
+    public List<Matrix.Entry<T>> scanTaxicabArea(MatrixPosition source, int distance) {
+        List<Matrix.Entry<T>> entries = new ArrayList<>();
+        for (int dr = -distance; dr <= distance; ++dr) {
+            int offset = distance - Math.abs(dr);
+            for (int dc = -offset; dc <= offset; ++dc) {
+                getEntry(source.move(dr, dc)).ifPresent(entries::add);
+            }
+        }
+        return entries;
     }
 
     public class MatrixIterator implements Iterator<T> {
@@ -249,6 +319,95 @@ public class Matrix<T> implements Iterable<T>, IMatrix<T> {
         }
     }
 
+    public List<Entry<T>> floodFill(int row, int col) {
+        return floodFill(new MatrixPosition(row, col));
+    }
+
+    public List<Entry<T>> floodFill(MatrixPosition source) {
+        return floodFillWithVisited(source).getA();
+    }
+
+    public Tuple<List<Entry<T>>, BinaryMatrix> floodFillWithVisited(MatrixPosition position) {
+        if (outOfBounds(position.row(), position.col())) {
+            throw new IndexOutOfBoundsException("Starting position [%d, %d] is out of bounds.".formatted(position.row(), position.col()));
+        }
+        List<MatrixPosition> visitedPositions = new ArrayList<>();
+        T target = get(position);
+
+        BinaryMatrix visited = new BinaryMatrix(this);
+        Queue<MatrixPosition> queue = new ArrayDeque<>();
+        queue.add(position);
+
+        // BFS
+        while (!queue.isEmpty()) {
+            MatrixPosition current = queue.poll();
+
+            if (outOfBounds(current) || visited.get(current) || !Objects.equals(get(current), target)) {
+                continue;
+            }
+
+            visited.set(current, true);
+            visitedPositions.add(current);
+
+            queue.add(current.move(Direction.NORTH));
+            queue.add(current.move(Direction.EAST));
+            queue.add(current.move(Direction.SOUTH));
+            queue.add(current.move(Direction.WEST));
+        }
+
+        var entries = visitedPositions.stream().map(pos -> Entry.of(pos, this.get(pos))).toList();
+        return new Tuple<>(entries, visited);
+    }
+
+    /**
+     * Recursive depth-first search with an output argument.
+     * @implNote Ignores {@code null} flood fills
+     */
+    private void floodFillHelper(T target, MatrixPosition current, BinaryMatrix visited, List<MatrixPosition> output, int depth) {
+        if (target == null || outOfBounds(current) || visited.get(current) || !Objects.equals(target, get(current))) {
+            return;
+        }
+        visited.set(current, true);
+        output.add(current);
+        for (Direction direction : Direction.getCardinal()) {
+            floodFillHelper(target, current.move(direction), visited, output, depth + 1);
+        }
+    }
+
+    public List<List<Entry<T>>> getAllConnectedComponents() {
+        List<List<Entry<T>>> allConnectedComponents = new ArrayList<>();
+        BinaryMatrix visited = new BinaryMatrix(this);
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+                if (visited.get(row, col)) {
+                    continue;
+                }
+                // New source node
+                var result = this.floodFillWithVisited(new MatrixPosition(row, col));
+                var resultCC = result.getA();
+                if (resultCC.isEmpty()) {
+                    // Shouldn't really happen
+                    continue;
+                }
+                // Update known components
+                visited.orEquals(result.getB());
+                allConnectedComponents.add(resultCC);
+            }
+        }
+        return allConnectedComponents;
+    }
+
+    public List<Entry<T>> getLargestConnectedComponent() {
+        var ccs = getAllConnectedComponents();
+        List<Entry<T>> largestCC = List.of();
+        for (var cc : ccs) {
+            if (cc.size() > largestCC.size()) {
+                largestCC = cc;
+            }
+        }
+        return largestCC;
+    }
+
     public Stream<Entry<T>> stream() {
         return IntStream.range(0, rows)
                 .boxed()
@@ -261,37 +420,27 @@ public class Matrix<T> implements Iterable<T>, IMatrix<T> {
         return stream().toList();
     }
 
-    public List<Entry<T>> groupElements(MatrixPosition position) {
-        if (outOfBounds(position.row(), position.col())) {
-            throw new IndexOutOfBoundsException("Starting coordinates [%d, %d] are out of bounds.".formatted(position.row(), position.col()));
+    public Optional<Entry<T>> getEntry(MatrixPosition pos) {
+        if (this.outOfBounds(pos)) {
+            return Optional.empty();
         }
+        return Optional.of(Entry.of(pos, this.get(pos)));
+    }
 
-        T initialValue = get(position); // Get the starting value
-        List<Entry<T>> groupedCells = new ArrayList<>();
-        Queue<MatrixPosition> queue = new LinkedList<>();
-        boolean[][] visited = new boolean[rows][cols];
+    public Stream<Entry<T>> filter(BinaryMatrix binaryMatrix) {
+        return this.stream().filter(entry -> binaryMatrix.get(entry.position()));
+    }
 
-        queue.add(position);
+    public Entry<T> find(T target) {
+        return stream()
+                .filter(entry -> Objects.equals(entry.value, target))
+                .findFirst()
+                .orElseThrow(NoSuchElementException::new);
+    }
 
-        while (!queue.isEmpty()) {
-            MatrixPosition current = queue.poll();
-            int row = current.row();
-            int col = current.col();
-
-            if (outOfBounds(row, col) || visited[row][col] || !Objects.equals(get(row, col), initialValue)) {
-                continue;
-            }
-
-            // Mark as visited and add to the result list
-            visited[row][col] = true;
-            groupedCells.add(new Entry<>(row, col, initialValue));
-
-            // Add neighbors to the queue
-            queue.add(current.move(Direction.NORTH));
-            queue.add(current.move(Direction.SOUTH));
-            queue.add(current.move(Direction.WEST));
-            queue.add(current.move(Direction.EAST));
-        }
-        return groupedCells;
+    public List<Entry<T>> findAll(T target) {
+        return stream()
+                .filter(entry -> Objects.equals(entry.value, target))
+                .toList();
     }
 }
