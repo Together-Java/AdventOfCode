6c6ec8225c5433041739e91ea4871952e8709287
Hayden Taylor <hayden@avuna.net>
2025-11-30 19:52:40 -0500

    Added generateAocYear Gradle task which creates package, includes package-info.java and all boilerplate classes for each day of the year

---
+++ src/main/java/org/togetherjava/aoc/solutions/y2025/Day01.java
@@ -0,0 +1,19 @@
+package org.togetherjava.aoc.solutions.y2025;
+
+import org.togetherjava.aoc.internal.puzzle.AdventDay;
+import org.togetherjava.aoc.internal.puzzle.PuzzleInput;
+import org.togetherjava.aoc.internal.puzzle.PuzzleSolution;
+
+@AdventDay(day=1)
+public class Day01 implements PuzzleSolution {
+
+    @Override
+    public Object part1(PuzzleInput input) {
+        return null;
+    }
+
+    @Override
+    public Object part2(PuzzleInput input) {
+        return null;
+    }
+}
